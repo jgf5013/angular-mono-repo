d73780e72ad2da6ef04b5a55ac185d09f4267e79
John Fisher <jgf5013@gmail.com>
2021-02-24 12:04:27 -0500

    adding routing

--- apps/support-tickets-interface/src/app/app.module.ts
+++ apps/support-tickets-interface/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
