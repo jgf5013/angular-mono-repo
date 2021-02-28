7d313fd35d1e45c5b56fb9cf45d9fe0c7da920de
John Fisher <jgf5013@gmail.com>
2021-02-28 17:21:32 -0500

    Responsive Design

---
+++ apps/support-tickets-interface/src/app/ticket/constants.ts
@@ -0,0 +1,19 @@
+import { INewSupportTicket } from './types';
+export enum PriorityType {
+    LOW      = 0,
+    HIGH     = 1,
+    CRITICAL = 2
+};
+
+export enum StatusType {
+    OPEN       = 0,
+    INPROGRESS = 1,
+    DONE       = 2
+};
+
+export enum SaveType {
+    CREATE,
+    UPDATE
+}
+
+export const initialTicket: INewSupportTicket = {};
